72dc97ae173fdb067e807118b6285a0b9dfacd72
devkahn <gaebok0331@gmail.com>
2023-03-26 00:11:34 +0900

    Tabs

---
+++ build15Pjts/Tabs/app.js
@@ -0,0 +1,22 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about-img');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click' , function(e){
+    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    if(id){
+        // remove active from other buttons
+        btns.forEach(function(btn){
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+        // hide other aricles
+        articles.forEach(function(article){
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+});
+
