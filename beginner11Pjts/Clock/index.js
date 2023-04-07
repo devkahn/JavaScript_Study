61803f393f308b8b36fefc83ecd9c574d800369f
devkahn <gaebok0331@gmail.com>
2023-04-07 17:22:40 +0900

    Clock - setInterval Function

--- beginner11Pjts/Clock/index.js
+++ beginner11Pjts/Clock/index.js
@@ -1,21 +1,21 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second");
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
 
-function setDate() {
-  const now = new Date();
 
-  const getSecond = now.getSeconds();
-  const getMinute = now.getMinutes();
-  const getHour = now.getHours();
+function updateClock(){
+  const currentDate = new Date();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+  const hourDeg = (hour / 12) * 360;
+  hourEl.style.transform = `rotate(${hourDeg}deg)`;
+  const minuteDeg = (minute / 60) * 360;
+  minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+  const secondDeg = (second / 60) * 360;
+  secondEl.style.transform = `rotate(${secondDeg}deg)`;
 
-  const secondDegree = (getSecond / 60) * 360;
-  const minuteDegree = (getMinute / 60) * 360;
-  const hourDegree = (getHour / 12) * 360;
-
-  second.style.transform = `rotate(${secondDegree}deg)`;
-  minute.style.transform = `rotate(${minuteDegree}deg)`;
-  hour.style.transform = `rotate(${hourDegree}deg)`;
+  console.log(hour, minute, second);
 }
 
-setInterval(setDate, 1000);
+setInterval(updateClock, 1000);
