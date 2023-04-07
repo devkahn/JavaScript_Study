b5e9a132d8b7c2c79b9f547c7633e883fca81318
devkahn <gaebok0331@gmail.com>
2023-04-07 19:00:05 +0900

    Mouse Event - Mouse Curser Position Display

---
+++ beginner11Pjts/MouseEvent/index.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+
+window.addEventListener('mousemove', (event) =>{
+    container.innerHTML = `
+            <div class="mouse-event">
+                ${event.clientX}
+                <h4>Mouse X position(px)</h4>
+            </div>
+            <div class="mouse-event">
+                ${event.clientY}
+                <h4>Mouse Y position(px)</h4>
+            </div>
+    
+    `;
+
+});
