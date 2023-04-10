f747a267a4f13e745e84b6d959fa2c973ef4c9ff
devkahn <gaebok0331@gmail.com>
2023-04-10 16:15:06 +0900

    Responsive Admin Dashboard - Nav Bar

--- Dashboard/ResponsiveAdmin/script.js
+++ Dashboard/ResponsiveAdmin/script.js
@@ -10,3 +10,48 @@ allSideMenu.forEach(item=>{
         li.classList.add('active');
     })
 })
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sideBar = document.querySelector('#sidebar');
+
+menuBar.addEventListener('click', function(){
+    console.log('menuClick');
+    sideBar.classList.toggle('hide');
+} )
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function(e){
+    if(window.innerWidth < 576){
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show')){
+            searchButtonIcon.classList.replace('bx-search','bx-x');
+        }
+        else{
+            searchButtonIcon.classList.replace('bx-x','bx-search');
+        }
+    }
+})
+
+
+if(window.innerWidth < 768) {
+    console.log('innerwidth<768');
+    sideBar.classList.add('hide');
+}
+else if(window.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x','bx-search');
+    searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function(){
+    if(this.innerWidth > 576) {
+        searchButtonIcon.classList.replace('bx-x','bx-search');
+        searchForm.classList.remove('show');
+    }
+})
