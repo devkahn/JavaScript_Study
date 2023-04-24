66c7df8a9b24661eb70589eac36294750f66ec22
devkahn <gaebok0331@gmail.com>
2023-04-24 16:07:02 +0900

    Games-Enermy Movement Pattern : enemy1 movement study

--- Games/Enemy_Movement_Patterns/script.js
+++ Games/Enemy_Movement_Patterns/script.js
@@ -4,34 +4,39 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
 
-let numberOfEnemies = 100;
-let enemiesArray = [];
+const numberOfEnemies = 10;
+const enemiesArray = [];
 
-/*
-enemy1  =  {
-    x:0,
-    y:0,
-    width:200,
-    height:200,
-}
-*/
+let gameFrame = 0;
+
+
 
 class Enemy {
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
-        this.speed = Math.random() * 4  - 2;
-        this.num = Date.now();
+        this.image = new Image();
+        this.image.src = './enemies/enemy1.png';
+        //this.speed = Math.random() * 4  - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight /2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        // animate sprites
+        if(gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ?  this.frame  = 0 : this.frame++;
+        }
+        
     }
     draw() {
-        if(this.num % 2 === 0) ctx.fillRect(this.x, this.y, this.width, this.height);
-        else ctx.strokeRect(this.x, this.y, this.width, this.height);
+        //ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -47,7 +52,7 @@ function animate() {
         enermy.update();
         enermy.draw();
     });
-
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
