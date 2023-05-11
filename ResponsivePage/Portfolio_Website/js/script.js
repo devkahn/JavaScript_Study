b595e43fb73bee3690b775d4500043c11e98cece
devkahn <gaebok0331@gmail.com>
2023-05-11 17:03:26 +0900

    Portfolio - Footer Section And Scroll Active Finish

---
+++ ResponsivePage/Portfolio_Website/js/script.js
@@ -0,0 +1,21 @@
+// scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    console.log("ON SCROLL!");
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id  = sec.getAttribute('id');
+        console.log(top);
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            })
+        }
+    })
+}
