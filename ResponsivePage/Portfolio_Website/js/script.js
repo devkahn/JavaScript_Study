234e31e3d29da501d8aa46750139760103c1fd68
devkahn <gaebok0331@gmail.com>
2023-05-11 17:08:56 +0900

    Portfolio - Sticky navbar Finish

--- ResponsivePage/Portfolio_Website/js/script.js
+++ ResponsivePage/Portfolio_Website/js/script.js
@@ -1,4 +1,4 @@
-// scroll sections active link
+/* ============= scroll sections active link ============= */
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,7 +15,11 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            })
-        }
-    })
+            });
+        };
+    });
+
+    /* ============= STICKY NAVBAR ============= */
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100)
 }
